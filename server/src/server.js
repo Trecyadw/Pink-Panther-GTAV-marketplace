7748c7a6b352db042f34ae57ca7cf5d1796dc9eb
Trecyadw <trecyadewikusuman@gmail.com>
2026-08-19 22:59:34 +0700

    Add files via upload

---
+++ server/src/server.js
@@ -0,0 +1,6 @@
+import app from './app.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
