e30abbc8953e5461e6b5ee9b4795ef917e36b900
Trecyadw <trecyadewikusuman@gmail.com>
2026-08-19 23:34:03 +0700

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -13,8 +13,8 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
-  credentials: true,
+    origin: ["https://pink-panther-gtav-marketplace-qxac.vercel.app", "http://localhost:3000", "http://localhost:5000"],
+    credentials: true
 }));
 app.use(express.json());
 
